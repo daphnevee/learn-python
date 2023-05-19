42c47a5e8e84f4729abd668bc891ed6ac8931513
daphne <127839925+daphnevee@users.noreply.github.com>
2023-05-19 18:41:33 +0800

    Update mean_median_mode.py

--- Day-0/mean_median_mode.py
+++ Day-0/mean_median_mode.py
@@ -1,5 +1,9 @@
 #Python 3 program to calculate Mean, Median, Mode
 
+"""
+Task: Given an array, X, of N integers, calculate and print the respective mean, median, and mode on separate lines. If your array contains more than one modal value, choose the numerically smallest one. 
+"""
+
 #Import libraries
 import numpy as np
 from scipy import stats
