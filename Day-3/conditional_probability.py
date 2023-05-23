00414c667f8521cc34f98d240769ae1e16c079a1
daphne <127839925+daphnevee@users.noreply.github.com>
2023-05-23 22:09:51 +0800

    Create conditional_probability.py

---
+++ Day-3/conditional_probability.py
@@ -0,0 +1,17 @@
+"""
+Manual Solution to calculate Conditional Probability
+
+Task: Suppose a family has 2 children, one of which is a boy.
+What is the probability that both children are boys?
+
+Choices:
+a. 1/3 -- Answer
+b. 2/3
+c. 1/2
+d. 1/9
+
+Solution:
+Sample Space(S) = {Boy & Boy, Boy & Girl, Girl & Boy, Girl & Girl}
+Event(A): both children are boys
+P(Boy & Boy) = 1/3
+"""
