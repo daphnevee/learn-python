913b8ac4cb50730b78608a99484bfadcee8982b2
daphne <127839925+daphnevee@users.noreply.github.com>
2023-05-23 22:20:45 +0800

    Create drawing_marbles.py

---
+++ Day-3/drawing_marbles.py
@@ -0,0 +1,20 @@
+"""
+Manual Solution to calculate Conditional Probability and Combinations and Permutations
+
+Task: A bag contains 3 red marbles and 4 blue marbles. Then, 2 marbles are
+drawn from the bag, at random, without replacement. If the first marble drawn
+is red, what is the probability that the second marble is blue?
+
+Choices:
+a. 1/12
+b. 1/6
+c. 7/12
+d. 2/3 -- Answer
+
+Solution:
+Event A -> the 2nd marble is blue
+P(A) = 4/6 = 2/3
+
+Explanation: Since the first marble chosen is already red, the bag contains 6 remaining
+marbles. The second marble must be blue and there are still 4 blue marbles in the bag.
+"""
