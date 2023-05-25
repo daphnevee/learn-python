769a26f9d3c6a5bfe4372261b5f42de57629c074
daphne <127839925+daphnevee@users.noreply.github.com>
2023-05-25 13:35:13 +0800

    Create binomial_distribution_i.py
    This version uses a recursive approach in calculating the factorial of a number.

---
+++ Day-4/binomial_distribution_i.py
@@ -0,0 +1,51 @@
+"""
+Python 3 program to calculate Binomial Distribution I (Version 1)
+
+Task: The ratio of boys to girls for babies born in Russian is 1.09 : 1.
+If there is 1 child born per bith, what proportion of Russian families
+with exactly 6 children will have at least 3 boys?
+
+Write a program to compute the answer using the above parameters.
+Then print your result, rounded to a scale of 3 decimal places (i.e., 1.234 format).
+
+Sample input: 
+a = 1.09
+b = 1
+
+Expected output:
+Binomial Distribution: 0.696
+"""
+#Factorial (Recursive approach)
+def factorial(n):
+    return 1 if (n==1 or n==0) else n * factorial(n - 1)
+
+#Binomial
+def binomial(x, n, p):
+    #Probability of failure of 1 trial q
+    q = 1 - p
+    #Calculate nCr
+    nCr = factorial(n) / (factorial(x) * factorial(n - x))
+    #Calculate binomial probability
+    binomial = nCr * (p**x) * q**(n - x)
+    return binomial
+
+#Main
+def main():
+    #User input for ratio of boys to girls for babies born in Russia
+    a, b = list(map(float, input().split()))
+    
+    #Hard-coded total no. of trials
+    n = 6 
+    
+    #Calculate Probability
+    p = a / (a + b)
+    
+    #Calculate Binomial Distribution
+    result = binomial(3, n, p) + binomial(4, n, p) + binomial(5, n, p) + binomial(6, n, p)
+    
+    #Print the output for Binomial Distribution 
+    print("%.3f" % result)
+
+#To run main() function
+if __name__ == "__main__":
+    main()
