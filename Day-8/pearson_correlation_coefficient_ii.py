c9568c46f013512ad7d151974a9390e26c7b2606
daphne <127839925+daphnevee@users.noreply.github.com>
2023-05-25 18:08:00 +0800

    Create pearson_correlation_coefficient_ii.py

---
+++ Day-8/pearson_correlation_coefficient_ii.py
@@ -0,0 +1,29 @@
+"""
+Manual solution to calculate the Pearson Correlation Coefficient
+
+Task: The regression line of y on x is 3x + 4y + 8 = 0, and the
+regression line of x on y is 4x + 3y + 7 = 0. What is the value
+of the Pearson correlation coefficient?
+
+Choices:
+a. 1
+b. -1
+c. 3/4
+d. -4/3
+e. 4/3
+f. -3/4 -- Answer
+
+Solution:
+Equation 1: 3x + 4y + 8 = 0
+4y = -3x - 8
+y = -3/4x - 8/4
+y = -3/4x - 2
+byx = -3/4 or -0.75
+
+Equation 2: 4x + 3y + 7 = 0
+4x = -3y - 7
+x = -3/4y - 7/4
+bxy = -3/4 or -0.75
+
+r = (+/-)sqrt(byx * bxy) = (+/-)sqrt(-3/4 * -3/4) = -3/4 or -0.75
+"""
