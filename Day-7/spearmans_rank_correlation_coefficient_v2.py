7a49abd3a351ec627d92d0d6e356b3a989c774c3
daphne <127839925+daphnevee@users.noreply.github.com>
2023-05-25 17:40:05 +0800

    Create spearmans_rank_correlation_coefficient_v2.py
    This version utilizes Python's built-in library for Statistical functions under SciPy (scipy.stats) in ranking the elements of the data set.

---
+++ Day-7/spearmans_rank_correlation_coefficient_v2.py
@@ -0,0 +1,56 @@
+"""
+Python 3 program to calculate Spearman's Rank Correlation Coefficient (Version 2)
+
+Task: Given two n-element data sets, X and Y, calculate the value of
+Spearman's rank correlation coefficient.
+
+Sample input: 
+n = 10
+x = 10 9.8 8 7.8 7.7 1.7 6 5 1.4 2
+y = 200 44 32 24 22 17 15 12 8 4
+
+Expected output:
+Spearman's Rank Correlation Coefficient: 0.903
+"""
+#Import libraries
+import math
+import scipy.stats as ss
+
+#Spearman's Rank Correlation Coefficient
+def spearman(n, p, q):  
+    #Sum of the difference bet. the respective values of Rank X & Rank Y
+    di = 0
+    for i in range(n):
+        di += (p[i] - q[i])**2
+    
+    #Calculate Spearman's Rank Correlation Coefficient
+    spearman = 1 - ((6 * di)/(n*(n**2 - 1)))
+    
+    return spearman
+
+#Main
+def main():
+    #User input for the no. of values in data sets X and Y
+    n = int(input())
+    
+    #User input for the elements of data set X
+    x = list(map(float, input().split())) 
+    
+    #User input for the elements of data set Y
+    y = list(map(float, input().split()))
+    
+    #Rank the elements of data set X
+    p = ss.rankdata(x)
+    
+    #Rank the elements of data set Y
+    q = ss.rankdata(y)
+    
+    #Calculate Spearman's Rank Correlation Coefficient for data sets X and Y
+    spearman_result = spearman(n, p, q)
+    
+    #Print the output for Spearman's Rank Correlation Coefficient
+    print("%.3f" % spearman_result)
+
+#To run main() function
+if __name__ == "__main__":
+    main()
