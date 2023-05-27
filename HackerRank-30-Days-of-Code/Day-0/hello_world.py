7a04d93919321090a00d33cf4d461361a98dd628
daphne <127839925+daphnevee@users.noreply.github.com>
2023-05-27 14:12:19 +0800

    Create hello_world.py

---
+++ HackerRank-30-Days-of-Code/Day-0/hello_world.py
@@ -0,0 +1,23 @@
+"""
+Python 3 program to print Hello, World.
+
+Task: To complete this challenge, you must save a line of input from stdin to a variable, 
+print Hello, World. on a single line, and finally print the value of your variable on a second line.
+
+Input:
+inputString = Welcome to 30 Days of Code!
+
+Output:
+inputString: Welcome to 30 Days of Code!
+"""
+#Hard-coded String
+input_string = "Hello, World."
+
+#User input for String
+inputString = input()
+
+#Print the first string (hard-coded)
+print(input_string)
+
+#Print the second string (user input)
+print(inputString)
